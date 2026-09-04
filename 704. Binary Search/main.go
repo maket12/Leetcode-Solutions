737438a4a4b5bccaffdf8b40eaef9479ac8dd2dc
Зябкин Владимир <vovvovin5124@gmail.com>
2026-09-04 23:12:28 +0700

    704

---
+++ 704. Binary Search/main.go
@@ -0,0 +1,14 @@
+func search(nums []int, target int) int {
+    left, right := 0, len(nums) - 1
+    for left <= right {
+        middle := (right + left) / 2
+        if nums[middle] == target {
+            return middle
+        } else if nums[middle] < target {
+            left = middle + 1
+        } else {
+            right = middle - 1
+        }
+    }
+    return -1
+}
