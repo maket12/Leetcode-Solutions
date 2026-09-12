323e1d190a6d02c52d18429dd167aed507029e33
Зябкин Владимир <vovvovin5124@gmail.com>
2026-09-12 20:09:59 +0700

    162

---
+++ 162. Find Peak Element/main.go
@@ -0,0 +1,15 @@
+func findPeakElement(nums []int) int {
+    left, right := 0, len(nums) - 1
+
+    for left < right {
+        middle := left + (right - left) / 2
+
+        if nums[middle] < nums[middle + 1] {
+            left = middle + 1
+        } else {
+            right = middle
+        }
+    }
+
+    return left
+}
